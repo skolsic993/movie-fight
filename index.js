283882140223772acdc5fe35fee5325fa9fe1826
Miloš Stojsavljević <skolsic993@gmail.com>
2023-04-18 19:55:56 +0200

    Add input and call api for movies

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "6fb65d2f",
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+
+  return (...arg) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    setTimeout(() => {
+      func.apply(null, arg);
+    }, delay);
+  };
+};
+
+const onInput = (event) => {
+  fetchData(event.target.value);
+};
+
+input.addEventListener("input", debounce(onInput, 500));
