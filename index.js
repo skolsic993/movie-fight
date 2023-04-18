53f7a02daace73d264850e14fbf939b1dfdd2832
Miloš Stojsavljević <skolsic993@gmail.com>
2023-04-18 20:23:44 +0200

    Add movies to the dropdown

--- index.js
+++ index.js
@@ -6,27 +6,43 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
 
-  return (...arg) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
+  dropdown.classList.add("is-active");
+  for (let movie of movies) {
+    const option = document.createElement("a");
 
-    setTimeout(() => {
-      func.apply(null, arg);
-    }, delay);
-  };
-};
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+        <img src="${movie.Poster}"  />
+        ${movie.Title}
+    `;
 
-const onInput = (event) => {
-  fetchData(event.target.value);
+    resultsWrapper.appendChild(option);
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
---
+++ utils.js
@@ -0,0 +1,13 @@
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+
+  return (...arg) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func.apply(null, arg);
+    }, delay);
+  };
+};
